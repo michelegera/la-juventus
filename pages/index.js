b9a97c4ad4d0156eed90e031bb6bb662381e27b6
Michele Gerarduzzi <hello@michelegera.dev>
2021-05-11 23:51:54 +0200

    Switch from CSS to style-components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,28 @@
 import Head from "next/head";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const CoachName = styled.span`
+  font-weight: bold;
+`;
 
 const Index = () => {
   // TODO: calculate number of days from date
@@ -6,6 +30,8 @@ const Index = () => {
 
   return (
     <>
+      <GlobalStyle />
+
       <Head>
         <title>Chi allena la Juventus?</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,7 +41,7 @@ const Index = () => {
         <header>{/* Logo */}</header>
         <main>
           <p>
-            <span>Andrea Pirlo</span> è l’allenatore della Juventus da{" "}
+            <CoachName>Andrea Pirlo</CoachName> è l’allenatore della Juventus da{" "}
             {daysInCharge} giorni
           </p>
         </main>
