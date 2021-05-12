8b6349ee1f05113b23ca25426a9caa3687b068ce
Michele Gerarduzzi <hello@michelegera.dev>
2021-05-12 09:56:57 +0200

    Add sponsor button to footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,13 +1,25 @@
+import { usePlausible } from "next-plausible";
+
 import * as S from "./footer.styles";
 
-const Footer = () => (
-  <S.Container>
-    <p>
-      Questo sito <em>non</em> è affiliato con Juventus Football Club. Tutti i
-      marchi riportati appartengono ai legittimi proprietari.
-    </p>
-    <S.Hashtag>#finoallafine</S.Hashtag>
-  </S.Container>
-);
+const Footer = () => {
+  const plausible = usePlausible();
+
+  return (
+    <S.Container>
+      <S.Disclaimer>
+        Questo sito <em>non</em> è affiliato con Juventus Football Club. Tutti i
+        marchi riportati appartengono ai legittimi proprietari.
+      </S.Disclaimer>
+      <S.Sponsor
+        href="https://buymeacoffee.com/michelegera"
+        onClick={() => plausible("sponsor")}
+      >
+        ☕️ Offrimi un caffè
+      </S.Sponsor>
+      <S.Hashtag>#finoallafine</S.Hashtag>
+    </S.Container>
+  );
+};
 
 export default Footer;
--- components/footer/footer.styles.js
+++ components/footer/footer.styles.js
@@ -2,12 +2,31 @@ import styled from "styled-components";
 
 export const Container = styled.footer`
   background: #000;
-  color: #999;
   text-align: center;
   padding: 1rem;
   font-size: 0.875rem;
   box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
     0 10px 10px -5px rgba(0, 0, 0, 0.04);
+`;
+
+export const Disclaimer = styled.p`
+  color: #999;
+`;
+
+export const Sponsor = styled.a`
+  background: transparent;
+  border: 1px solid #ff2850;
+  color: #ff2850;
+  padding: 0.5rem 1rem;
+  display: inline-block;
+  cursor: pointer;
+  transition: all 250ms ease-in-out;
+
+  &:hover {
+    background: #ff2850;
+    color: #000;
+    transition: all 250ms ease-in-out;
+  }
 `;
 
 export const Hashtag = styled.p`
